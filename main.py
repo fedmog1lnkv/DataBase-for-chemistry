5176ab4fa304f67967a8126d2f90bbc79572ba6c
fedmog1lnkv <kuzneczov-f@susanoo.ru>
2022-08-17 13:45:33 +0800

    добавлены новые страницы
    созданы парсеры основывающиеся на индексах предыдущих выборов для среды, температуры и давления

--- main.py
+++ main.py
@@ -2,41 +2,101 @@ from tkinter import *
 from tkinter import ttk
 from parser import Parser
 
-Ecxel = Parser()
-
-print("выберите нужное вещество:", *(Ecxel.parse_substance()))
-
 class app:
     def __init__(self, master):
         self.master = master
         self.master.geometry("200x200")
-        self.login()
+        self.Ecxel = Parser()
+        self.start()
 
-    def login(self):
+    def start(self):
         for i in self.master.winfo_children():
             i.destroy()
         self.frame1 = Frame(self.master, width=300, height=300)
         self.frame1.pack()
-        self.text = ttk.Label(self.frame1, text='Поиск веществ')
+        self.text = ttk.Label(self.frame1, text ="Поиск веществ")
         self.text.pack()
-        self.next_btn = ttk.Button(self.frame1, text="Начать", command=self.register)
+        self.next_btn = ttk.Button(self.frame1, text="Начать", command=self.page_1)
         self.next_btn.pack()
 
-    def register(self):
+    def page_1(self):
         for i in self.master.winfo_children():
             i.destroy()
 
         self.frame2 = Frame(self.master, width=300, height=300)
         self.frame2.pack()
-        self.text = ttk.Label(self.frame2, text='Выберите вещество')
+
+        self.text = ttk.Label(self.frame2, text="Выберите вещество")
         self.text.pack()
-        self.choice_substance = ttk.Combobox(self.frame2, values=Ecxel.parse_substance())
+
+        self.choice_substance = ttk.Combobox(self.frame2, values=self.Ecxel.parse_substance())
         self.choice_substance.pack()
-        self.next_btn = ttk.Button(self.frame2, text="Далее", command=self.login)
+
+        self.next_btn = ttk.Button(self.frame2, text="Далее", command=self.page_2)
         self.next_btn.pack()
 
 
 
+    def page_2(self):
+        self.choice_substance = self.choice_substance.get()
+
+        for i in self.master.winfo_children():
+            i.destroy()
+
+        self.frame3 = Frame(self.master, width=300, height=300)
+        self.frame3.pack()
+
+        self.text = ttk.Label(self.frame3, text="Выберите среду")
+        self.text.pack()
+
+        self.choice_env = ttk.Combobox(self.frame3, values=self.Ecxel.parse_env(self.choice_substance))
+        self.choice_env.pack()
+
+        self.next_btn = ttk.Button(self.frame3, text="Далее", command=self.page_3)
+        self.next_btn.pack()
+
+    def page_3(self):
+        self.choice_env = self.choice_env.get()
+
+        for i in self.master.winfo_children():
+            i.destroy()
+
+        self.frame4 = Frame(self.master, width=300, height=300)
+        self.frame4.pack()
+
+        self.text = ttk.Label(self.frame4, text="Выберите температуру")
+        self.text.pack()
+
+        self.choice_temp = ttk.Combobox(self.frame4, values=self.Ecxel.parse_temp(self.choice_env))
+        self.choice_temp.pack()
+
+        self.next_btn = ttk.Button(self.frame4, text="Далее", command=self.page_4)
+        self.next_btn.pack()
+
+    def page_4(self):
+        self.choice_temp = self.choice_temp.get()
+
+        for i in self.master.winfo_children():
+            i.destroy()
+
+        self.frame5 = Frame(self.master, width=300, height=300)
+        self.frame5.pack()
+
+        self.text = ttk.Label(self.frame5, text="Выберите давление")
+        self.text.pack()
+
+        self.choice_press = ttk.Combobox(self.frame5, values=self.Ecxel.parse_press(self.choice_temp))
+        self.choice_press.pack()
+
+        self.next_btn = ttk.Button(self.frame5, text="Далее", command=self.page_4)
+        self.next_btn.pack()
+
+    def page_5(self):
+        self.choice_press = self.choice_press.get()
+        print(self.choice_press)
+
+        for i in self.master.winfo_children():
+            i.destroy()
 window = Tk()
 app(window)
 window.mainloop()
--- parser.py
+++ parser.py
@@ -8,20 +8,82 @@ class Parser:
         self.sheet = self.DB['Лист1']
         self.parse_substance()
 
-
     def parse_substance(self):
-        self.about_substance = []
+        self.aboutSubstance = []
         countOFF = 0
         for i in range(2, 100000):
             if self.sheet["A" + str(i)].value != None:
-                #TODO: двумерный список для нахождения диапазонов температур и т.д.
-                self.about_substance += [self.sheet["A" + str(i)].value, "A" + str(i)]
+                # TODO: двумерный список для нахождения диапазонов температур и т.д.
+                self.aboutSubstance += [[self.sheet["A" + str(i)].value, str(i)]]
                 countOFF = 0
             elif countOFF == 50:
                 break
             else:
                 countOFF += 1
-        return self.about_substance
+        self.allSubstance = []
+        for i in range(len(self.aboutSubstance)):
+            self.allSubstance += [self.aboutSubstance[i][0]]
+        return self.allSubstance
 
-    def parse_temperature(self):
-        pass
+    def parse_env(self, substance):
+
+        self.indexChoice = []
+        for i in range(len(self.aboutSubstance)):
+            if self.aboutSubstance[i][0] == substance:
+                # self.allEnv += [self.sheet["B" + self.aboutSubstance[i][1]].value]
+                self.indexChoice += [self.aboutSubstance[i][1], self.aboutSubstance[i + 1][1]]
+                break
+
+        self.aboutEnv = []
+
+        for i in range(int(self.indexChoice[0]), int(self.indexChoice[1])):
+            if self.sheet["B" + str(i)].value != None:
+                self.aboutEnv += [[self.sheet["B" + str(i)].value, str(i)]]
+
+        self.allEnv = []
+        for i in range(len(self.aboutEnv)):
+            self.allEnv += [self.aboutEnv[i][0]]
+        return self.allEnv
+
+    def parse_temp(self, env):
+
+        for i in range(len(self.allEnv)):
+            if self.allEnv[i] == env:
+                start = int(self.aboutEnv[i][1])
+
+        j = start + 1
+        stop = start + 1
+        while self.sheet["B" + str(j)].value == None:
+            stop += 1
+            j += 1
+
+        self.aboutTemp = []
+        for i in range(start, stop):
+            if self.sheet["C" + str(i)].value != None:
+                self.aboutTemp += [[self.sheet["C" + str(i)].value, str(i)]]
+        self.allTemp = []
+        for i in range(len(self.aboutTemp)):
+            self.allTemp += [self.aboutTemp[i][0]]
+        return self.allTemp
+
+    def parse_press(self, temp):
+
+        for i in range(len(self.allTemp)):
+            if self.allTemp[i] == temp:
+                start = int(self.aboutTemp[i][1])
+
+        j = start + 1
+        stop = start + 1
+        while self.sheet["C" + str(j)].value == None:
+            stop += 1
+            j += 1
+
+        self.aboutPress = []
+        print(stop)
+        for i in range(start, stop):
+            if self.sheet["D" + str(i)].value != None:
+                self.aboutPress += [[self.sheet["D" + str(i)].value, str(i)]]
+        self.allPress = []
+        for i in range(len(self.aboutPress)):
+            self.allPress += [self.aboutPress[i][0]]
+        return self.allPress
